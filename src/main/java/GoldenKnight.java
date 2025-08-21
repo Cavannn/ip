55e2a70e00820ca14c5439ca41af29e2ddd26ba1
Cavan <cavanchua0975@gmail.com>
2025-08-21 20:46:27 +0800

    Level 0 implementation

---
+++ src/main/java/GoldenKnight.java
@@ -0,0 +1,18 @@
+public class Duke {
+    public static void main(String[] args) {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+
+        String Lvl0 = "_______________________________________\n"
+                + "Hello! I'm the Golden Knight! \n"
+                + "What can I do for you? \n"
+                + "_______________________________________\n"
+                + " Bye. Hope to see you again soon! \n"
+                + "_______________________________________\n";
+
+        System.out.println(Lvl0);
+    }
+}
