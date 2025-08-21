cf5df0ce26b39138d07ad047e680dbbd28538b74
Cavan <cavanchua0975@gmail.com>
2025-08-22 03:50:55 +0800

    level 3 implementation

--- src/main/java/GoldenKnight.java
+++ src/main/java/GoldenKnight.java
@@ -5,7 +5,7 @@ public class GoldenKnight {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> items = new ArrayList<>();
+        ArrayList<Task> items = new ArrayList<>();
 
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -35,21 +35,40 @@ public class GoldenKnight {
 
         while (true) {
             String input = scanner.nextLine();
+            String[] parts = input.split(" ", 2);
+            String command = parts[0];
 
-            // Exit
-            if (input.equals("bye")) {
+            if (command.equals("bye")) {
                 System.out.println(goodbye);
                 break;
-            } else if (input.equals("list")) {
+            } else if (command.equals("list")) {
                 System.out.println(line);
+                System.out.println(" Here are the tasks in your list:");
                 for (int i = 0; i < items.size(); i++) {
                     System.out.println(" " + (i + 1) + ". " + items.get(i));
                 }
                 System.out.println(line);
-            } else {
-                items.add(input);
+            } else if (command.equals("mark")) {
+                int taskNo = Integer.parseInt(parts[1]) - 1;
+                Task task = items.get(taskNo);
+                task.markAsDone();
                 System.out.println(line);
-                System.out.println(" added: " + input);
+                System.out.println(" Nice! I've marked this task as done:");
+                System.out.println(task);
+                System.out.println(line);
+            } else if (command.equals("unmark")) {
+                int taskNo = Integer.parseInt(parts[1]) - 1;
+                Task task = items.get(taskNo);
+                task.MarkAsNotDone();
+                System.out.println(line);
+                System.out.println(" OK, I've marked this task as not done yet:");
+                System.out.println(task);
+                System.out.println(line);
+            } else {
+                Task newTask = new Task(input);
+                items.add(newTask);
+                System.out.println(line);
+                System.out.println(" added: " + newTask);
                 System.out.println(line);
             }
         }
---
+++ src/main/java/Task.java
@@ -0,0 +1,29 @@
+public class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatus() {
+        return (isDone
+                ? "X"
+                : " ");
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void MarkAsNotDone() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + getStatus() + "] " + description;
+    }
+
+}
