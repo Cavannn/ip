d50893ff09bc3f284009200f12f0201a5b4ea830
Cavan <cavanchua0975@gmail.com>
2025-08-22 03:35:13 +0800

    level 2 implementation

--- src/main/java/GoldenKnight.java
+++ src/main/java/GoldenKnight.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class GoldenKnight {
     public static void main(String[] args) {
 
-        Scanner scanner =  new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> items = new ArrayList<>();
 
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -27,6 +29,8 @@ public class GoldenKnight {
                 + " Bye. Hope to see you again soon! \n"
                 + "_______________________________________\n";
 
+        String line = "_______________________________________\n";
+
         System.out.print(greeting);
 
         while (true) {
@@ -36,15 +40,21 @@ public class GoldenKnight {
             if (input.equals("bye")) {
                 System.out.println(goodbye);
                 break;
+            } else if (input.equals("list")) {
+                System.out.println(line);
+                for (int i = 0; i < items.size(); i++) {
+                    System.out.println(" " + (i + 1) + ". " + items.get(i));
+                }
+                System.out.println(line);
+            } else {
+                items.add(input);
+                System.out.println(line);
+                System.out.println(" added: " + input);
+                System.out.println(line);
             }
-
-            System.out.println("_______________________________________\n"
-                    + " " + input + "\n"
-                    + "_______________________________________\n");
         }
 
         scanner.close();
 
     }
-
 }
